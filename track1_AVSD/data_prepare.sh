1ff5298972c15fa943c8e90b0509f6aeae7c8dba
mispchallenge <88126124+mispchallenge@users.noreply.github.com>
2022-11-10 18:44:06 +0800

    Update data_prepare.sh

--- track1_AVSD/data_prepare.sh
+++ track1_AVSD/data_prepare.sh
@@ -6,8 +6,8 @@ stage=1
 set=dev  # Defining the set: train, dev, or eval.
 
 audio_path=/export/corpus/misp2022/Released/audio/${set}/far/  # Defining original audio data path
-wpe_save_path=/wpe/${set}  # Defining path to save WPE result
-session_list=/scp_dir/${set}.list  # A list containing all sessions to be processed. Each line is the name of the session, like: R11_S193194_C08_I2
+wpe_save_path=wpe/${set}  # Defining path to save WPE result
+session_list=scp_dir/${set}.list  # A list containing all sessions to be processed. Each line is the name of the session, like: R11_S193194_C08_I2
 
 mkdir -p $wpe_save_path
 
@@ -20,7 +20,7 @@ fi
 
 video_dir=/export/corpus/misp2022/Released/video/${set}/far/  # Defining video data path
 roi_json_dir=/export/corpus/misp2022/Released/roi_coordinates/${set}/far/lip/  # Defining detection_results path
-roi_store_dir=/detection_roi/${set}/far  # Defining path to save face and lip ROIs
+roi_store_dir=detection_roi/${set}/far  # Defining path to save face and lip ROIs
 
 mkdir -p $roi_store_dir
 
