08a1793455079e812adeef9edde7e112319df6fc
Shilong Wu <117905504+slwu0209@users.noreply.github.com>
2022-11-14 22:47:14 +0800

    Update path.sh

--- track1_AVSD/path.sh
+++ track1_AVSD/path.sh
@@ -1,5 +1,5 @@
 export KALDI_ROOT=`pwd`/../../..      # Defining Kaldi root directory  e.g. /home/backman/kaldi
-[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+#[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/bin/:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
